976b4c2c66caa8424a927d4e5e9d9c9a2697e155
Josh Wilson <joshua.wilson6289@gmail.com>
2020-12-10 15:35:16 -0600

    got router working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar/>
+      <Route exact path="/index.html" component={Home} />
+      <Route exact path="/contact.html" component={Contact} />
+      <Route exact path="/portfolio.html" component={Portfolio} />
+
+
+
+
+
+    </Router>
   );
 }
 
