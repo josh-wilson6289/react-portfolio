7845aea2c2b0b46238458490e332fc3ce667f016
Josh Wilson <joshua.wilson6289@gmail.com>
2021-03-10 12:16:27 -0600

    added icons. rephrased some text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,22 @@ function Footer(){
     <nav className="navbar navbar-dark bg-dark fixed-bottom">
       <ul className="list-group list-group-horizontal nav navbar-nav">
         <li className="nav-item">
-          <a className="nav-link" href="https://www.linkedin.com/in/joshwilsonsound/" target="_blank">LinkedIn</a>
+          <a className="nav-link" href="https://www.linkedin.com/in/joshwilsonsound/" target="_blank">
+          <img src="/react-portfolio/Assets/linkedin.png" href="https://www.linkedin.com/in/joshwilsonsound/" target="_blank" className="footer-icon"></img>
+          LinkedIn
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="https://github.com/josh-wilson6289" target="_blank">GitHub</a>
+        <a className="nav-link" href="https://github.com/josh-wilson6289" target="_blank">
+        <img src="/react-portfolio/Assets/GitHub.png" href="https://github.com/josh-wilson6289" target="_blank" className="footer-icon"></img>
+         GitHub
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/react-portfolio/Assets/Josh_Wilson_Resume.pdf" target="_blank">Resume</a>
+          <a className="nav-link" href="/react-portfolio/Assets/Josh_Wilson_Resume.pdf" target="_blank">
+          <img src="/react-portfolio/Assets/resume.png" href="/react-portfolio/Assets/Josh_Wilson_Resume.pdf" target="_blank" className="footer-icon"></img>
+            Resume
+            </a>
         </li>
       </ul>
     </nav>
--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../App.css";
- 
 
 function Jumbotron() {
   return(
@@ -13,19 +12,20 @@ function Jumbotron() {
             </div>
             <div className="col-md-9">
               <h1 className="display-8">Hello, my name is Josh Wilson.</h1>
-              <p className="lead">
+              <p className="lead" id="description">
                 I'm a self-motivated full-stack web developer with proficiency in Javascript, HTML, and CSS.
                 One of my greatest strengths is my willingness to adapt and work with a team, while learning new skills quickly to accomplish the task at hand.
                 I graduated from the University of Texas Coding Bootcamp, and bring over a decade of experience as an Audio Engineer
-                for film, commercials, and TV.  
-                Apologies for any of the commercials I mixed that may have annoyed you over the years.
+                for film, television, and advertising.  
+                Sorry for any of the annoying commercials I helped produce over the years.
                 In my spare time, I enjoy homebrewing, playing hockey, and adopting way too many pets.
               </p>
+              <br></br>
                 <p className="center lead" id="tech">Technologies Used</p>
                 <p className="lead" id="smaller-text">  
                 JavaScript, ES6, React, Node, Express, 
                 MongoDB, Mongoose, MySQL, Sequelize, HTML5, CSS3, Bootstrap, 
-                Semantic-UI, jQuery, RESTful APIs, GIT, GitHub, Heroku
+                Semantic-UI, jQuery, RESTful API, Git, GitHub, Heroku
               </p>
             </div>
            
