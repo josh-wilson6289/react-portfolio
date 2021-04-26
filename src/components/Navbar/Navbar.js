bb652a54089c962680d69d6833a68cb006a233d1
Josh Wilson <joshua.wilson6289@gmail.com>
2021-04-26 14:47:38 -0500

    fixed navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import "./style.css";
 
 function Navbar() {
   return (
     <header className="navigation">
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <Link className="navbar-brand" to="index.html">Josh Wilson</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link className="nav-link" to="portfolio.html">Portfolio</Link>
@@ -20,7 +14,7 @@ function Navbar() {
             <Link className="nav-link" to="contact.html">Contact</Link>
           </li>
         </ul>
-      </div>
+
     </nav>
   </header>
   )
